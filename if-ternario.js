a4e4d738b85aea430948a3e97df9391786da29e7
nec2dev <nec2multivackergithub@outlook.com>
2021-03-08 19:36:00 -0300

    if ternario

--- if-ternario.js
+++ if-ternario.js
@@ -1,17 +1,29 @@
 console.log('If ternario:');
 
-let camisa = {
+/*let camisa = {
     talla: 34,
     cantidad: 0,
     stock: false
+}*/
+
+let camisa = {
+    talla: 34,
+    cantidad: 12,
+    stock: true
 }
 
 const {talla, cantidad, stock} = camisa;
 
+/*If normal*/
 if(stock){
-    respuesta = 'Hay ' + cantidad + 'de camisas';
+    respuesta = 'Hay ' + cantidad + ' camisas';
 }else{
     respuesta = 'No hay en stock';
 }
 
+console.log('respuesta: ', respuesta);
+
+/*if ternario: sirve para hacer comprobaciones rapidamente
+de valores booleanos*/
+stock ? respuesta = 'Hay ' + cantidad + ' camisas' : respuesta = 'No hay en stock';
 console.log('respuesta: ', respuesta);
