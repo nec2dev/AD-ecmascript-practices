f0eeeb99035d180cfca8b1907407d92790092604
nec2dev <nec2multivackergithub@outlook.com>
2021-03-08 19:43:34 -0300

    literal templates

--- literal-templates.js
+++ literal-templates.js
@@ -1,1 +1,9 @@
 console.log('Plantillas literales:');
+
+/*Template literales: Sirven para concatrenar variables sin
+interrumpir la cadena de string con comillas*/
+
+let estudiante = 'Juan';
+console.log('El estudiante se llama ' + estudiante + ' , y cursa backend');
+/*Template literales*/
+console.log(`El estudiante se llama ${estudiante}, y cursa backend`);
